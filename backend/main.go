7acb4af5d7f8116f8da7058b746ae393f3b2b216
MadRobin13 <madrobin1340@gmail.com>
2024-08-27 18:13:34 -0400

    moved backend methods to websocket.go

--- backend/main.go
+++ backend/main.go
@@ -2,65 +2,31 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github/MadRobin13/Go-and-React-Website/pkg/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
 
-	CheckOrigin: func(r *http.Request) bool {return true},
-}
 
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
 
-		fmt.Println(p)
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-	}
-}
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
 
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setUpRoutes() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintf(w, "Simple Server")
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			fmt.Errorf("Error reading body: %v", err)
-		}
-		defer r.Body.Close()
-
-		fmt.Fprintf(w, "message: %v\n", string(body))
-	})
-	
 	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Chattr v0.01")
 	setUpRoutes()
 	http.ListenAndServe(":8080", nil)
 }

f4907ff3abb1b80f1129c580d459755f529e63fe
MadRobin13 <madrobin1340@gmail.com>
2024-08-28 16:18:50 -0400

    fixed pool.go errors

--- backend/pkg/websocket/pool.go
+++ backend/pkg/websocket/pool.go
@@ -7,7 +7,7 @@ import (
 type Pool struct {
 	Register chan *Client
 	Unregister chan *Client
-	Clients chan map[*Client]bool
+	Clients map[*Client]bool
 	Broadcast chan Message
 }
 
@@ -26,7 +26,7 @@ func (pool *Pool) Start() {
 		case client := <- pool.Register:
 			pool.Clients[client] = true
 			fmt.Println("size of connecton pool: ", len(pool.Clients))
-			for client ,_ := range pool.Clients {
+			for client, _ := range pool.Clients {
 				fmt.Println(client)
 				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
 			} 

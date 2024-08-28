c5c1eec2950c3119f4cb40ad7474b517df3f17b3
MadRobin13 <madrobin1340@gmail.com>
2024-08-28 15:16:36 -0400

    Adding Client Pool but has errors

---
+++ backend/pkg/websocket/pool.go
@@ -0,0 +1,35 @@
+package websocket
+
+import (
+	"fmt"
+)
+
+type Pool struct {
+	Register chan *Client
+	Unregister chan *Client
+	Clients chan map[*Client]bool
+	Broadcast chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool {
+		Register: make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients: make(map[*Client]bool),
+		Broadcast: make(chan Message),
+	}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <- pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("size of connecton pool: ", len(pool.Clients))
+			for client ,_ := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Joined..."})
+			} 
+		}
+	}
+}
